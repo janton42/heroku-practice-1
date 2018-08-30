52e630417fe044040d05434ac1cfea0832b5959e
janton42 <jantonstock@gmail.com>
2018-08-29 21:53:42 -0700

    basic layout with nav bar fixed to the bottom, smooth scrolling, several sections, and a background image; place-holder buttons

---
+++ public/Scripts/script.js
@@ -0,0 +1,16 @@
+var eventList = {
+	events: [],
+	schedule: function(){
+		alert('Connect to Doodle');
+	},
+	invite: function(){
+		alert('Connect to Calendar API');
+	},
+	restaurants: function(){
+		alert('Connect to Yelp API');
+	},
+	login: function(){
+		alert('Log in view');
+	}
+}
+
