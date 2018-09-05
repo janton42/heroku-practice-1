6eb0e5107b691be9f234759689dc964419532bea
janton42 <jantonstock@gmail.com>
2018-09-04 22:26:51 -0700

    provisioned postgresql db

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+});
+
+app.get('db', async (req, res) => {
+	try {
+		const client = await pool.connect()
+		const result = await client.query('SELEC * FROM test_table');
+		res.render('pages/db', result);
+		client.release();
+	} catch (err) {
+		console.error(err);
+		res.send("Error " + err);
+	}
+});
 //set port for application
 
 const port = process.env.PORT || 8080;
