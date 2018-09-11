515f321ce25514eb0877d63ee717c21ed26228a6
janton42 <jantonstock@gmail.com>
2018-09-10 23:06:15 -0700

    set up backend functions

--- server.js
+++ server.js
@@ -1,34 +1,71 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
+const PORT = 8080;
 
-//set port for application
-const port = process.env.PORT || 8080;
-
-//set view engine to ejs
+// //set view engine to ejs
 app.set('view engine', 'ejs');
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect = ('mongodb://localhost/alphaFriends');
+mongoose.connect('mongodb://localhost/CRMdb');
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// routes(app);
+routes(app);
 
-//tell express to look in the public directory for assets
+// //tell express to look in the public directory for assets
 app.use(express.static(__dirname + '/public'));
 
-//set home page route
-app.get('/', function(req, res) {
-	//ejs render will always look in the views folder automatically
-	res.render('index');
-});
+app.get('/', (req, res) =>
+    res.render('index')
+);
 
-app.listen(port, function(){
-	console.log('Our app is running on http://localhost:' + port);
-});
+app.listen(PORT, () =>
+    console.log(`your server is running on port ${PORT}`)
+);
+
+
+
+
+
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import bodyParser from 'body-parser';
+// import routes from './src/routes/crmRoutes';
+
+// const app = express();
+
+// //set port for application
+// const port = process.env.PORT || 8080;
+
+// //set view engine to ejs
+// app.set('view engine', 'ejs');
+
+// // mongoose connection
+// mongoose.Promise = global.Promise;
+// mongoose.connect = ('mongodb://localhost/alphaFriends');
+
+// // bodyparser setup
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// routes(app);
+
+// //tell express to look in the public directory for assets
+// app.use(express.static(__dirname + '/public'));
+
+// //set home page route
+// app.get('/', function(req, res) {
+// 	//ejs render will always look in the views folder automatically
+// 	res.render('index');
+// });
+
+// app.listen(port, function(){
+// 	console.log('Our app is running on http://localhost:' + port);
+// });
