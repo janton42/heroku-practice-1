cddf37ee43206197111d61174c3450880a208c8d
janton42 <jantonstock@gmail.com>
2018-09-10 19:28:17 -0700

    added mongoose connection and setup bodyparser

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-const { Pool } = require('pg');
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: true
-});
-
-app.get('db', async (req, res) => {
-	try {
-		const client = await pool.connect()
-		const result = await client.query('SELEC * FROM test_table');
-		res.render('pages/db', result);
-		client.release();
-	} catch (err) {
-		console.error(err);
-		res.send("Error " + err);
-	}
-});
 //set port for application
-
 const port = process.env.PORT || 8080;
 
 //set view engine to ejs
 app.set('view engine', 'ejs');
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect = ('mongodb://localhost/alphaFriends');
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
 
 //tell express to look in the public directory for assets
 app.use(express.static(__dirname + '/public'));
